8fc1a123e26fb7ef4b41016389798ffad31b10a3
jebo <jbouhi@octo.com>
2020-06-19 09:47:05 +0200

    Create afficheur repository + impl

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Hotel } from './domain/hotel'
-import { AfficheurRepositoryImpl } from './infrastructure/afficheur-repository-impl'
+import { AfficheurRepositoryImpl } from './application/afficheur-repository-impl'
 import { ChambresInMemoryRepository } from './infrastructure/chambres-in-memory-repository'
 
 const chambreRepository = new ChambresInMemoryRepository();
-const hotel = new Hotel(chambreRepository);
 const afficheur = new AfficheurRepositoryImpl()
+const hotel = new Hotel(chambreRepository);
 afficheur.execute(hotel.afficherToutesLesChambres())
