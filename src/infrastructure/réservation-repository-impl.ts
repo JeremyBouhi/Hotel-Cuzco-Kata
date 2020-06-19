8c528a2af0d52a70bbabf1494e5afa2f2516bc78
jebo <jbouhi@octo.com>
2020-06-19 15:15:57 +0200

    Tests + Implem de l historique des reservations dans le fichier json

--- src/infrastructure/réservation-repository-impl.ts
+++ src/infrastructure/réservation-repository-impl.ts
@@ -1,8 +1,11 @@
 import { RéservationRepository } from '../core/port/réservation-repository'
 import { Réservation } from '../core/entités/réservation'
+import * as fs from 'fs'
+import * as path from 'path';
 
 export class RéservationRepositoryImpl implements RéservationRepository {
-    enregistrer(réservation: Réservation):void {
-
+    enregistrer(réservation: Réservation): void {
+        const filePath = './historique-des-réservations.json'
+        fs.writeFileSync(path.resolve(__dirname, filePath), JSON.stringify(réservation))
     }
 }
--- test/infrastructure/réservation-repository-impl.test.ts
+++ test/infrastructure/réservation-repository-impl.test.ts
@@ -1,26 +1,20 @@
 import {RéservationRepositoryImpl} from '../../src/infrastructure/réservation-repository-impl';
 import {Réservation} from '../../src/core/entités/réservation';
 import * as fs from 'fs';
+import * as path from 'path';
 
 describe(`enregistrer`, ()=> {
-    it(`vérifie que la réservation est bien enregistré dans le fichier json`, ()=> {
+    it(`vérifie que la réservation est bien enregistré dans le fichier json`, async ()=> {
         //given
         const réservationRepository = new RéservationRepositoryImpl();
         const réservation = new Réservation(new Date(2020,6,19), new Date(2020,6,20), 4, 102);
+        const filePath = '../../src/infrastructure/historique-des-réservations.json';
 
         //when
         réservationRepository.enregistrer(réservation);
+
         //then
-        const filePath = '../../src/infrastructure/historique-des-réservations.json';
-        let result:string;
-        fs.readFile(filePath, (err, réservations) => {
-            try {
-            console.log(réservations);
-                //result = JSON.parse(réservations.toString())
-            } catch(err) {
-                console.log(err);
-            }
-        })
-        expect(result).toEqual('');
+        const result = JSON.parse(fs.readFileSync(path.resolve(__dirname, filePath)).toString());
+        expect(result).toEqual({"checkIn": "2020-07-18T22:00:00.000Z", "checkOut": "2020-07-19T22:00:00.000Z", "nombreDeVoyageurs": 4, "numéroChambre": 102});
     });
 });
